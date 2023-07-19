a7069455cfa74d58363926977314a5c2e9a8ed73
Devashree Gosavi <35646522+devashreegosavi@users.noreply.github.com>
2023-07-19 13:14:12 +0530

    19 july 2023

---
+++ src/app/recipes/recipe.service.ts
@@ -0,0 +1,17 @@
+import { EventEmitter } from '@angular/core';
+import { Recipe } from './recipe.model';
+
+export class RecipeService{
+
+    recipeSelected = new EventEmitter<Recipe>();
+    private recipes : Recipe[] = [
+        new Recipe('A Test Recipe one','This is a simply Test one',
+        'https://img.delicious.com.au/Iok992Gi/w759-h506-cfill/del/2022/02/chicken-chickpea-curry-163942-1.jpg'),
+        new Recipe('A Test Recipe two','This is a simply Test two',
+        'https://img.delicious.com.au/Iok992Gi/w759-h506-cfill/del/2022/02/chicken-chickpea-curry-163942-1.jpg')
+      ];
+
+      getRecipes(){
+        return this.recipes.slice();
+      }
+}
